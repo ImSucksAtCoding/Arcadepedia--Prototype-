0eb59ae75bba0cdd565f5077e81105a62ce83006
Linh-Nguyen <s4034535@rmit.edu.vn>
2024-05-16 18:54:15 +0700

    modified:   article.css 	modified:   article.html 	modified:   article.js

--- article.js
+++ article.js
@@ -1,15 +1,45 @@
- // Select all elements with the "i" tag and store them in a NodeList called "stars"
- const stars = document.querySelectorAll(".stars i");
+function myFunction1() {
+  var x = document.getElementById("paragraph1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    
+  } else {
+    x.style.display = "none";
+  }
+}
 
- // Loop through the "stars" NodeList
- stars.forEach((star, index1) => {
-   // Add an event listener that runs a function when the "click" event is triggered
-   star.addEventListener("click", () => {
-     // Loop through the "stars" NodeList Again
-     stars.forEach((star, index2) => {
-       // Add the "active" class to the clicked star and any stars with a lower index
-       // and remove the "active" class from any stars with a higher index
-       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-     });
-   });
- });
+function myFunction2() {
+  var x = document.getElementById("paragraph2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("paragraph3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("paragraph4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction5() {
+  var x = document.getElementById("paragraph5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
